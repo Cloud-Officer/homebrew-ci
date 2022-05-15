183fa1d262bcf85f8c76d79f1bec857a03d64243
Yves Desgagné <yves@cloudofficer.ca>
2022-05-15 17:39:16 -0400

    Added ci-tools

---
+++ ci-tools.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class CITools < Formula
+  desc 'Continuous Integration tools'
+  homepage 'https://github.com/Cloud-Officer/ci-tools'
+  url 'https://github.com/Cloud-Officer/ci-tools.git',
+      tag: '0.0.1'
+  head 'https://github.com/Cloud-Officer/soup.git'
+
+  depends_on 'actionlint'
+  depends_on 'awscli'
+  depends_on 'bandit'
+  depends_on 'flake8'
+  depends_on 'golangci-lint'
+  depends_on 'hadolint'
+  depends_on 'ktlint'
+  depends_on 'markdownlint-cli'
+  depends_on 'php-cs-fixer'
+  depends_on 'pmd'
+  depends_on 'ruby'
+  depends_on 'shellcheck'
+  depends_on 'swiftlint'
+  depends_on 'yamllint'
+
+  def install
+    bin.install 'linters'
+    bin.install 'ssh-jump'
+  end
+end
