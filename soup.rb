224820e9c4792d947c8a3e06eb0505032c42e0a4
Yves Desgagné <yves@cloudofficer.ca>
2022-05-17 14:50:49 -0400

    Bumped soup to 0.0.7

--- soup.rb
+++ soup.rb
@@ -4,7 +4,7 @@ class Soup < Formula
   desc 'Software of Unknown Provenance'
   homepage 'https://github.com/Cloud-Officer/soup'
   url 'https://github.com/Cloud-Officer/soup.git',
-      tag: '0.0.6'
+      tag: '0.0.7'
   head 'https://github.com/Cloud-Officer/soup.git'
 
   depends_on 'ruby'
