ab4e53507b805e1318f8d0238ea918b582b76eaa
Yves Desgagné <yves@cloudofficer.ca>
2022-06-07 20:27:02 -0400

    Added deployment script

--- citools.rb
+++ citools.rb
@@ -4,7 +4,7 @@ class Citools < Formula
   desc 'Continuous Integration tools'
   homepage 'https://github.com/Cloud-Officer/ci-tools'
   url 'https://github.com/Cloud-Officer/ci-tools.git',
-      tag: '0.0.1'
+      tag: '0.0.2'
   head 'https://github.com/Cloud-Officer/soup.git'
 
   depends_on 'actionlint'
@@ -22,8 +22,38 @@ class Citools < Formula
   depends_on 'swiftlint'
   depends_on 'yamllint'
 
+  resource 'json' do
+    url 'https://rubygems.org/gems/aws-sdk-3.1.0.gem'
+    sha256 'fecbc05fceb55c162ce99e3cc27208021af83eadf3fe19359136d9ab70561279'
+  end
+
+  resource 'optparse' do
+    url 'https://rubygems.org/gems/optparse-0.2.0.gem'
+    sha256 '9dea1f5278397e8862af3333bd73ca146e2675bfac8b7e24227db01275894175'
+  end
+
   def install
+    bin.install('deploy.rb')
     bin.install('linters')
     bin.install('ssh-jump')
+    (lib / 'citools/vendor').mkpath
+
+    resources.each do |r|
+      r.verify_download_integrity(r.fetch)
+      system('gem', 'install', r.cached_download, '--no-document', '--install-dir', "#{lib}/citools/vendor")
+    end
+
+    rm_rf('vendor')
+
+    (bin / 'deploy').write(exec_script)
+  end
+
+  def exec_script
+    <<~SHELL
+      #!/usr/bin/env bash
+      export GEM_HOME="#{HOMEBREW_PREFIX}/lib/citools/vendor"
+      export DISABLE_BUNDLER_SETUP=1
+      exec ruby "#{HOMEBREW_PREFIX}/bin/deploy.rb" "$@"
+    SHELL
   end
 end
