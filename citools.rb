01afcec940db56b2c718cfd4621af6355a9848ac
Yves Desgagné <yves@cloudofficer.ca>
2022-08-02 16:50:22 -0400

    Bumped citool to 0.0.4

--- citools.rb
+++ citools.rb
@@ -4,7 +4,7 @@ class Citools < Formula
   desc 'Continuous Integration tools'
   homepage 'https://github.com/Cloud-Officer/ci-tools'
   url 'https://github.com/Cloud-Officer/ci-tools.git',
-      tag: '0.0.3'
+      tag: '0.0.4'
   head 'https://github.com/Cloud-Officer/soup.git'
 
   depends_on 'actionlint'
