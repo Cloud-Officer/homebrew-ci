9b82f044c99c051dbbae7af1990c2dcbf599c5b6
Yves Desgagné <yves@cloudofficer.ca>
2023-10-12 17:46:53 -0400

    Update soup.rb

--- soup.rb
+++ soup.rb
@@ -10,21 +10,14 @@ class Soup < Formula
   depends_on 'ruby'
 
   def install
-    ENV["GEM_HOME"] = libexec
-    ENV["BUNDLE_PATH"] = libexec
-    system "gem", "install", "bundler"
-    system "bundle", "install"
+    system 'gem', 'install', 'bundler'
+    system 'bundle', 'install'
 
     prefix.install(Dir['bin'])
     prefix.install(Dir['config'])
     prefix.install(Dir['lib'])
     prefix.install(Dir['Gemfile*'])
-    bin.install_symlink(libexec/'bin/soup')
-    (bin / 'soup').write(exec_script)
-  end
-
-  def exec_script
-    <<~SHELL
+    (bin/'soup').write <<~SHELL
       #!/usr/bin/env bash
       exec "#{Formula["ruby"].opt_bin}/ruby" "#{HOMEBREW_PREFIX}/bin/soup.rb" "$@"
     SHELL
