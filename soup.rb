cb01eb065115650187be7fcfe1c93774db8737ee
Yves Desgagné <yves@cloudofficer.ca>
2023-10-16 13:21:20 -0400

    Update soup.rb

--- soup.rb
+++ soup.rb
@@ -11,16 +11,18 @@ class Soup < Formula
 
   def install
     ENV["GEM_HOME"] = libexec
+    ENV["GEM_PATH"] = libexec
     ENV["BUNDLE_PATH"] = libexec
     system 'gem', 'install', 'bundler'
     system 'bundle', 'install'
     prefix.install(Dir['*'])
-    ruby_version = Utils.safe_popen_read("#{Formula["ruby"].opt_bin}/ruby", "-e", "puts RUBY_VERSION").chomp
 
     (bin/'soup').write <<~SHELL
       #!/usr/bin/env bash
       export GEM_HOME="#{libexec}"
-      exec "#{Formula['ruby'].opt_bin}/ruby" -W0 -I#{libexec}/lib/ruby/#{ruby_version} -e 'require "rubygems"; require "bundler/setup"; load "#{bin}/soup.rb"' "$@"
+      export GEM_PATH="#{libexec}"
+      export BUNDLE_PATH="#{libexec}"
+      exec "#{Formula["ruby"].opt_bin}/ruby" "#{bin}/soup.rb" "$@"
     SHELL
   end
 end
