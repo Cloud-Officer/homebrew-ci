bf3ed96021b1ee2822d19cea1ba8e1e49c105dd8
Tommy Lacroix <tommy@nuagelab.com>
2025-11-25 21:12:43 -0500

    Add ssm-jump

--- citools.rb
+++ citools.rb
@@ -4,7 +4,7 @@ class Citools < Formula
   desc 'Continuous Integration tools'
   homepage 'https://github.com/Cloud-Officer/ci-tools'
   url 'https://github.com/Cloud-Officer/ci-tools.git',
-      tag: '1.2.30'
+      tag: '1.2.32'
   head 'https://github.com/Cloud-Officer/soup.git'
 
   depends_on 'actionlint'
