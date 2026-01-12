d71ea3900c92a88c3ae3767b53c2f4807c3a5cac
Yves Desgagné <yves@cloudofficer.ca>
2026-01-12 16:28:44 -0500

    Bump citools to 1.3.4, githubbuild to 1.8.0, soup to 1.4.41 (#67)

--- githubbuild.rb
+++ githubbuild.rb
@@ -4,14 +4,14 @@ class Githubbuild < Formula
   desc 'GitHub build file generator'
   homepage 'https://github.com/Cloud-Officer/github-build'
   url 'https://github.com/Cloud-Officer/github-build.git',
-      tag: '1.7.1'
+      tag: '1.8.0'
   head 'https://github.com/Cloud-Officer/github-build.git'
 
   depends_on 'ruby'
 
   resource 'activesupport' do
-    url 'https://rubygems.org/gems/activesupport-8.1.1.gem'
-    sha256 '5e92534e8d0c8b8b5e6b16789c69dbea65c1d7b752269f71a39422e9546cea67'
+    url 'https://rubygems.org/gems/activesupport-8.1.2.gem'
+    sha256 '88842578ccd0d40f658289b0e8c842acfe9af751afee2e0744a7873f50b6fdae'
   end
 
   resource 'ast' do
@@ -100,8 +100,8 @@ class Githubbuild < Formula
   end
 
   resource 'multi_xml' do
-    url 'https://rubygems.org/gems/multi_xml-0.8.0.gem'
-    sha256 '8d4adcd092f8e354db496109829ffd36969fdc8392cb5fde398ca800d9e6df73'
+    url 'https://rubygems.org/gems/multi_xml-0.8.1.gem'
+    sha256 'addba0290bac34e9088bfe73dc4878530297a82a7bbd66cb44dcd0a4b86edf5a'
   end
 
   resource 'open3' do
@@ -180,8 +180,8 @@ class Githubbuild < Formula
   end
 
   resource 'rubocop-rspec' do
-    url 'https://rubygems.org/gems/rubocop-rspec-3.8.0.gem'
-    sha256 '28440dccb3f223a9938ca1f946bd3438275b8c6c156dab909e2cb8bc424cab33'
+    url 'https://rubygems.org/gems/rubocop-rspec-3.9.0.gem'
+    sha256 '8fa70a3619408237d789aeecfb9beef40576acc855173e60939d63332fdb55e2'
   end
 
   resource 'rubocop-thread_safety' do
