07baa6a033a1b152fc36cc18d795a3f938bbcc7f
Yves Desgagné <yves@cloudofficer.ca>
2026-04-13 10:52:54 -0400

    Bump citools 1.7.2, githubbuild 1.18.2, soup 1.6.17

--- citools.rb
+++ citools.rb
@@ -4,7 +4,7 @@ class Citools < Formula
   desc 'Continuous Integration tools'
   homepage 'https://github.com/Cloud-Officer/ci-tools'
   url 'https://github.com/Cloud-Officer/ci-tools.git',
-      tag: '1.7.1'
+      tag: '1.7.2'
   head 'https://github.com/Cloud-Officer/ci-tools.git'
 
   depends_on 'actionlint'
