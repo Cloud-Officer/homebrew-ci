bdc48f514c264f063aa9f85bc8cb057a676b0b3f
Yves Desgagné <yves@cloudofficer.ca>
2026-08-24 15:19:12 -0400

    Bump citools 1.7.38, githubbuild 1.25.4, soup 1.9.23 and gem resources

--- soup.rb
+++ soup.rb
@@ -4,7 +4,7 @@ class Soup < Formula
   desc 'Software of Unknown Provenance'
   homepage 'https://github.com/Cloud-Officer/soup'
   url 'https://github.com/Cloud-Officer/soup.git',
-      tag: '1.9.22'
+      tag: '1.9.23'
   head 'https://github.com/Cloud-Officer/soup.git'
 
   depends_on 'ruby'
@@ -186,8 +186,8 @@ class Soup < Formula
   end
 
   resource 'rubocop' do
-    url 'https://rubygems.org/gems/rubocop-1.89.0.gem'
-    sha256 '4dee8e3ee9c45e474834efd9e8d6fd031e8331c8dacdff0de4ad65ae0a6faae7'
+    url 'https://rubygems.org/gems/rubocop-1.90.0.gem'
+    sha256 '9eb4c065b5c5154e4ef554c547972f3905a9eb6b53e657e580b6796b54bf8242'
   end
 
   resource 'rubocop-ast' do
@@ -201,8 +201,8 @@ class Soup < Formula
   end
 
   resource 'rubocop-graphql' do
-    url 'https://rubygems.org/gems/rubocop-graphql-1.7.0.gem'
-    sha256 'e78636d6e28d5852dce7cf863bda35e1d659f57dfeea1cd6f71f972751f25142'
+    url 'https://rubygems.org/gems/rubocop-graphql-1.8.0.gem'
+    sha256 'b3714aa5d9bbd1680d928439de71a0aa21e31318a0ae808d591ba8b40eb94745'
   end
 
   resource 'rubocop-minitest' do
@@ -271,8 +271,8 @@ class Soup < Formula
   end
 
   resource 'webmock' do
-    url 'https://rubygems.org/gems/webmock-3.26.2.gem'
-    sha256 '774556f2ea6371846cca68c01769b2eac0d134492d21f6d0ab5dd643965a4c90'
+    url 'https://rubygems.org/gems/webmock-3.26.3.gem'
+    sha256 '77c612816bdcb2fc43db0e42fd98b19be7d4770fd708816fdde65a006664ddec'
   end
 
   resource 'wisper' do
