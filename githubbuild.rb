bc950a14cf22760f6052f2254a552e94f77a79c5
Yves Desgagné <yves@cloudofficer.ca>
2026-09-16 13:35:09 -0400

    #151: satisfy brew audit on license placement, hash alignment and line length

--- githubbuild.rb
+++ githubbuild.rb
@@ -4,10 +4,10 @@ class Githubbuild < Formula
   desc 'GitHub build file generator'
   homepage 'https://github.com/Cloud-Officer/github-build'
   url 'https://github.com/Cloud-Officer/github-build.git',
-      tag: '1.29.0',
+      tag:      '1.29.0',
       revision: '85df82f3c3ad4a741b68cfe4e4475a762153c787'
-  head 'https://github.com/Cloud-Officer/github-build.git'
   license 'MIT'
+  head 'https://github.com/Cloud-Officer/github-build.git'
 
   depends_on 'ruby'
 
